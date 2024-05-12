18e61ddc8da3137a8f93dd75165dd222fd0773d0
Rachel Struthers <rstruthers1@yahoo.com>
2024-05-12 17:13:12 -0500

    Removed logging of db uri. Added to .gitignore

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 const MONGO_DB_URI: string = process.env.MONGO_DB_URI || '';
 
-console.log(`MONGO_DB_URI: ${MONGO_DB_URI}`);
-
 export const connectDB = async () => {
     try {
         await mongoose.connect(MONGO_DB_URI);
